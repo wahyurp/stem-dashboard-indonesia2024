c58f412089f57a6cdbf5f595cc7fafa0637a8529
wahyurp <wahyu.rahmaditama@gmail.com>
2025-09-11 13:09:20 +0700

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -743,7 +743,7 @@ card_html = """
               box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
               font-size:18px; font-style:italic; color:#333;
               width:80%; margin:40px auto; text-align:center;">
-              “In 2024, the distribution of STEM graduates in Indonesia shows that the largest shares are concentrated in provinces like Jawa Barat (16.73%) and Jawa Timur (12.99%), reflecting the dominance of Java as the country’s STEM hub. Male and female shares are relatively balanced with provincial differences. ”
+              “In 2024, the distribution of STEM graduates in Indonesia shows that the largest shares are concentrated in provinces like <b>Jawa Barat (16.73%)</b> and <b>Jawa Timur (12.99%)</b>, reflecting the dominance of Java as the country’s STEM hub. Male and female shares are relatively balanced with provincial differences. ”
           </div>
         </div>
       </div>

a0afd3e0472ca8746b60719a6d2eb910989e0b6e
wahyurp <wahyu.rahmaditama@gmail.com>
2025-09-11 11:23:50 +0700

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -736,11 +736,12 @@ card_html = """
             </a>
           </div>
         </div>
+        <div class="content-wrap" style="margin-top:24px">
+          <div id="choropleth" style="width:100%;height:500px;"></div>
+        </div>
       </div>
     </div>
-    <div class="content-wrap" style="margin-top:24px">
-      <div id="choropleth" style="width:100%;height:500px;"></div>
-    </div>
+
     <!-- Materialize JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://cdn.plot.ly/plotly-2.30.0.min.js"></script>

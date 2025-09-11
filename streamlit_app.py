b6c1c0079943cb31724c1204a29774a9e04cf00e
wahyurp <wahyu.rahmaditama@gmail.com>
2025-09-11 13:14:21 +0700

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -736,13 +736,12 @@ card_html = """
             </a>
           </div>
         </div>
-        <div class="content-wrap" style="margin-top:10%">
+        <div class="content-wrap" style="margin-top:10%; margin-bottom:10%;">
          <div class="title-text blue-text" id="topic-title">Map STEM Graduates</div>
           <div id="choropleth" style="width:100%;height:500px;"></div>
           <div style="background-color:#f0f0f0; padding:20px; border-radius:10px;
               box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
               font-size:18px; font-style:italic; color:#333;
-              margin-bottom:90px;
               width:80%; margin:40px auto; text-align:center;">
               “In 2024, the distribution of STEM graduates in Indonesia shows that the largest shares are concentrated in provinces like <b>Jawa Barat (16.73%)</b> and <b>Jawa Timur (12.99%)</b>, reflecting the dominance of Java as the country’s STEM hub. Male and female shares are relatively balanced with provincial differences. ”
           </div>
